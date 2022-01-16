4dd4b30d5fb589ab515f7b38cf9741842a1f27c1
anatoliy.zhuchkov <anatoliy.zhuchkov@myoffice.team>
2022-01-17 02:43:41 +0300

    refactoring most common func

--- python_webcount.py
+++ python_webcount.py
@@ -1,16 +1,19 @@
 import requests
 
 
-def most_common_worl_in_web_page(words, url):
-    response = requests.get(url)
-    text = response.text
-
+def most_common_word(words: list, text: str):
     word_frequency = {wrd: text.count(wrd) for wrd in words}
     return sorted(words, key=word_frequency.get)[-1]
 
 
+def most_common_word_in_web_page(words, url):
+    response = requests.get(url)
+    text = response.text
+    return most_common_word(words, text)
+
+
 if __name__ == "__main__":
-    most_common = most_common_worl_in_web_page(
+    most_common = most_common_word_in_web_page(
         ['python', 'Python', 'programming'],
         'https://python.org'
     )
