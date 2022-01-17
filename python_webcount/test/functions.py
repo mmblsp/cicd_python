b60a97306ed0a1f625c99401d4b07aa2ee29e13c
anatoliy.zhuchkov <anatoliy.zhuchkov@myoffice.team>
2022-01-17 06:15:57 +0300

    add mock object for tested user agent

--- python_webcount/test/functions.py
+++ python_webcount/test/functions.py
@@ -5,23 +5,26 @@ def test_most_common_word():
     assert most_common_word(['a', 'b', 'c', 'd'], 'abbbcc') \
         == 'b', 'most_common_word with unique asnwer'
 
+
 def test_most_common_word_empty_candidate():
     from pytest import raises
     with raises(Exception):
         most_common_word([], 'abc')
 
+
 def test_most_common_ambiguous_result():
     assert most_common_word(['a', 'b', 'c'], 'ab') \
         in ('a', 'b'), "there might be a tie"
 
+
 def test_with_test_double():
     class TestResponse():
         text = 'aa bbb c'
-    
+
     class TestUserAgent():
         def get(self, url):
             return TestResponse()
-    
+
     result = most_common_word_in_web_page(
         ['a', 'b', 'c'],
         'https://python.org',
@@ -29,3 +32,17 @@ def test_with_test_double():
     )
     assert result == 'b', \
         'most_common_in_web_page tested with test double'
+
+
+def test_with_test_mock():
+    from unittest.mock import Mock
+    mock_requests = Mock()
+    mock_requests.get.return_value.text = 'aa bbb c'
+
+    result = most_common_word_in_web_page(
+        ['a', 'b', 'c'],
+        'https://python.org',
+        user_agent=mock_requests
+    )
+    assert result == 'b',\
+        'most_common_word_in_web_page tested with test double'
